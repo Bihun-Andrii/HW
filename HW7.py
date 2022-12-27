b972234476a48844793d624886c08d130d9c7d73
Bihun Andrii <andreibegun@gmail.com>
2022-12-27 23:12:19 +0200

    updated home work lesson 7

--- HW7.py
+++ HW7.py
@@ -3,37 +3,48 @@ phone_book = {
     'Diana': 254345,
 }
 
-print (phone_book.keys())
+print (phone_book.keys(),phone_book.values())
 
 while True:
     user_input = input('Enter a command: ')
     split_input = user_input.split()
    
     command = split_input[0]
-    name = split_input[1]
-    number = split_input[2]
- 
-    if phone_book.get(name) is None:  #тут у мене щось не виходить - як зробити, щоб можно було ввести лише command?
-        phone_book[name] = 0
-        
+    
+    if len(split_input) > 1:
+        name = split_input[1]
+    else:
+        name = None
+
+    if len(split_input) > 2:
+        number = split_input[2]
+    else:
+        number = None
+    
     if command == 'stats':
         print(len(phone_book))
 
     elif command == 'add':
-        phone_book[name] = number
-        print (phone_book.keys())
+        if name in phone_book:
+           print(f"{name} is exist, please enter another name")
+        else:
+            phone_book[name] = number
+            print (phone_book.keys())
         
     elif command == 'delete':
-        del phone_book[name]
-        print(phone_book.keys())
+        if phone_book.get(name) == None:
+           print(f"{name} is missing, please enter an existing name")
+        else:
+           del phone_book[name]
+           print(phone_book.keys())
     
     elif command == 'list':
-        print (phone_book.keys())
+        if len(split_input) == 1:
+           name = None
+           print (phone_book.keys())
 
-    elif command == 'show':
-        for key, value in phone_book.items():
-            print (key)
-            print(value) 
+    elif command == 'show':   
+        print (phone_book[name])    
 
 #Створити телефонну книгу, яка матиме наступні команди:
 #stats: кількість записів
