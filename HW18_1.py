97ece6fbea97d7d31918aab528bf658a8cb1ceed
Bihun Andrii <andreibegun@gmail.com>
2023-02-08 21:08:15 +0200

    My_homework_Lesson_18

---
+++ HW18_1.py
@@ -0,0 +1,18 @@
+class Bot:
+    def __init__(self, name):
+        self.name = name
+       
+    def say_name(self):
+        print(self.name)
+
+    def send_message (self, message):
+        self.message = message
+        print(message)
+
+some_bot = Bot('Marvin')
+some_bot.say_name()
+some_bot.send_message('Hello')
+
+# 1. Створити клас Bot з атрибутом name та методами say_name та send_message. 
+# send_message має приймати параметри self і message і має друкувати message. 
+# Метод say_name має друкувати значення атрибуту name.
